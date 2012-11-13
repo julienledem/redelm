d66480b211cc5a6ebe7c5432cb7b7f212c0d8e3b
Jonathan Coveney <jcoveney@gmail.com>
2012-11-13 09:56:32 -0800

    Passes tests

---
+++ redelm-column/src/test/java/redelm/column/primitive/BitWriter.java
@@ -0,0 +1,21 @@
+package redelm.column.primitive;
+
+public class BitWriter {
+  public BitWriter()
+
+  public void writeBit(boolean val) {
+
+  }
+
+  public void writeByte(int val) {
+
+  }
+
+  public void writeBits(int val, int bitsToWrite) {
+
+  }
+
+  public byte[] getByteArray() {
+
+  }
+}
